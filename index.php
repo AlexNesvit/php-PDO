0a71cfdb591f33202d18ee500c215d4a2b9cebc3
Afpa <alex.nesvit2000@gmail.com>
2024-03-18 16:06:04 +0100

    formulaire v2.0

--- index.php
+++ index.php
@@ -2,18 +2,25 @@
 <html lang="fr">
 <head>
     <meta charset="UTF-8">
+    <link rel="stylesheet" href="style.css">
     <title>Form Inscription</title>
 </head>
 <body>
     <form method="POST">
         <label for="nom">Nom:</label>
-        <input type="text" id="nom" name="nom">
-        <button type="submit" name="submit">Envoyer</button>
+        <input  class="text" type="text" id="nom" name="nom"><br><br>
+        <label for="mail">Mail:</label>
+        <input class="text"type="text" id="mail" name="mail"><br><br>
+        <label for="password">Mot de pass:</label>
+        <input class="text"type="text" id="password" name="password"><br><br>
+        <button type="submit" class="button" name="submit">Envoyer</button>
     </form>
 </body>
 </html>
 
 <?php
+
+/*
     // Paramètres de connexion à la base de données MySQL
 $dsn = 'mysql:host=localhost:3306;dbname=bd-pdo';
 $user = 'root';
@@ -40,17 +47,65 @@ if(isset($_POST['nom'])){
     // Подготовка и выполнение SQL-запроса для вставки имени пользователя в таблицу "user"
     // Préparation et exécution de la requête SQL pour insérer le nom de l'utilisateur dans la table "user"
 
-    /*
-    $sql = "INSERT INTO user (nom) VALUES (:nom)";
+    
+    $sql = "INSERT INTO user (nom, mail, password) VALUES (:nom, :mail, :password)";
     $stmt = $pdo->prepare($sql);
-    $stmt->bindParam(':nom',$nomuser);
+    $stmt->bindParam(':nom', $nomuser);
+    $stmt->bindParam(':mail', $mail);
+    $stmt->bindParam(':password', $password);
     $stmt->execute();
+    */
+
+    session_start();
+
+// Vérifie si le formulaire a été soumis
+if(isset($_POST['submit'])) {
+    // Vérifie si les champs requis sont renseignés
+    if(!empty($_POST['nom']) && !empty($_POST['mail']) && !empty($_POST['password'])) {
+        // Récupère les valeurs du formulaire
+        $nomuser = $_POST['nom'];
+        $mail = $_POST['mail'];
+        $password = $_POST['password'];
+
+        // Connexion à la base de données
+        $dsn = 'mysql:host=localhost;dbname=bd-pdo';
+        $user = 'root';
+        $password = '';
+
+        try {
+            $pdo = new PDO($dsn, $user, $password);
+            $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+            // Prépare la requête SQL d'insertion
+            $sql = "INSERT INTO user (nom, mail, password) VALUES (:nom, :mail, :password)";
+            $stmt = $pdo->prepare($sql);
+
+            // Lie les paramètres
+            $stmt->bindParam(':nom', $nomuser);
+            $stmt->bindParam(':mail', $mail);
+            $stmt->bindParam(':password', $password);
+
+            // Exécute la requête
+            $stmt->execute();
+
+            // Affiche un message de succès
+            echo "<p class='success-message'>Nouvel utilisateur ajouté.Données insérées avec succès!";
+        } catch(PDOException $e) {
+            // Affiche les erreurs
+            echo "Erreur : " . $e->getMessage();
+        }
+    } else {
+        // Affiche un message si des champs requis sont manquants
+        echo "<p class='alert'>Veuillez remplir tous les champs!";
+    }
+}
+
 
     // Вывод сообщения об успешном добавлении данных
     // Affichage d'un message de succès pour l'ajout des données
+    /*
     echo "Ok ajouté";
-
-    */
+    
     // Vérification si le nom existe déjà dans la base de données
     $sql_check = "SELECT COUNT(*) FROM user WHERE nom = :nom";
     $stmt_check = $pdo->prepare($sql_check);
@@ -88,5 +143,5 @@ if(isset($_POST['nom'])){
     }
     echo "</ul>";
 }
-
+*/
 ?>
